a1481dc033edcb990b5b6679d8a45625c140fb2c
phso6981 <soderstromphilip@gmail.com>
2026-04-05 09:57:00 +0200

    Changed to english in commented code in generate_signal

--- rule_based_strategy.py
+++ rule_based_strategy.py
@@ -9,10 +9,10 @@ class RuleBasedStrategy:
     # Runs on an individual row in real time or during backtest.
     # Checks at the latest row and desides whether to buy, sell or hold based on the indicators
     def generate_signal(self, row):
-        cond1 = row["price_to_sma"] > 0                  # Price over SMA200
-        cond2 = row["sma_cross"] > 0                        # SMA50 over SMA200
-        cond3 = 35 <= row["rsi"] <= 65                            # RSI neutral
-        cond4 = row["obv_diff"] > 0                        # OBV stiger      
+        cond1 = row["price_to_sma"] > 0     # Price over SMA200
+        cond2 = row["sma_cross"] > 0        # SMA50 over SMA200
+        cond3 = 35 <= row["rsi"] <= 65      # RSI neutral
+        cond4 = row["obv_diff"] > 0         # OBV rising      
         
         if cond1 and cond2 and cond3 and cond4:
             return 1    # Buy signal
